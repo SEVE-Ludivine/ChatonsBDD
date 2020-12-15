cb66a572df86279ab7e8b7dae593a95763b0d86f
Ludivine <ludivine.seve@epsi.fr>
2020-12-15 13:18:27 +0100

    Ajoutez des fichiers projet.

---
+++ ChatonsBDD.ORM/Categorie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatonsBDD.ORM
+{
+    public class Categorie
+    {
+
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Libelle { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Chaton> Chatons { get; } = new List<Chaton>();
+    }
+}
---
+++ ChatonsBDD.ORM/Chaton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ChatonsBDD.ORM
+{
+    public class Chaton
+    {
+        public int Id { get; set; }
+
+        //Les dataannotations vont permettre d'ajouter des contraintes pour la création de la BDD
+        [MaxLength(50)]
+        public string Nom { get; set; }
+
+
+        //Le ? veut dire Nullable<DateTime>
+        public DateTime? DateDeNaissance { get; set; }
+
+
+        public bool? Sterilise { get; set; }
+
+
+        public bool? EstUneFemelle { get; set; }
+
+        public int CategorieId { get; set; }
+
+        public Categorie Categorie { get; set; }
+
+
+
+
+
+    }
+}
